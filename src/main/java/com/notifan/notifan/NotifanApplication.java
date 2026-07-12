d133bb2682d0b7e6dab68eedde8e504e07301055
rzfrsn <tafita.raza1@gmail.com>
2026-07-12 19:20:40 +0200

    chore : initial Spring Boot Scaffold

---
+++ src/main/java/com/notifan/notifan/NotifanApplication.java
@@ -0,0 +1,13 @@
+package com.notifan.notifan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifan/notifan/NotifanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifan.notifan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
